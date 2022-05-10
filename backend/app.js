5a35a337f742f09caee85005166556acbddce87f
Anej Krajnc <anej.krajnc@hotmail.com>
2022-05-10 21:28:55 +0200

    Added packages, routes and MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,36 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require("mongoose");
+var mongoDB = "mongodb://127.0.0.1/projekt";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/userRoutes');
+var parcelLockersRouter = require('./routes/parcelLockerRoutes');
+var accessPermissionsRouter = require('./routes/accessPermissionRoutes');
+var unlocksRouter = require('./routes/unlockedRoutes');
+var eNotifyRouter = require('./routes/eNotifyRoutes');
 
 var app = express();
+
+var cors = require('cors');
+var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+app.use(cors({
+  credentials: true,
+  origin: function(origin, callback){
+    // Allow requests with no origin (mobile apps, curl)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin)===-1){
+      var msg = "The CORS policy does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +45,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+app.use(session({
+  secret: 'amigos',
+  resave: true,
+  saveUninitialized: false,
+  store: MongoStore.create({mongoUrl: mongoDB})
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/parcel-lockers', parcelLockersRouter);
+app.use('/access-permissions', accessPermissionsRouter);
+app.use('/unlocks', unlocksRouter);
+app.use('/notifications', eNotifyRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
